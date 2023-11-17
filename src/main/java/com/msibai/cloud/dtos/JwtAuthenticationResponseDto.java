b4cd5ada097ceeff092f96d6dcdc830574f2c30e
Mukhtar Sibai <mukhtar@Mukhtars-MBP.local>
2023-11-17 21:45:08 +0100

    Create JwtAuthenticationResponseDto class

---
+++ src/main/java/com/msibai/cloud/dtos/JwtAuthenticationResponseDto.java
@@ -0,0 +1,14 @@
+package com.msibai.cloud.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponseDto {
+  private String token;
+}
