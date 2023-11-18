f305c538d320eaa4f5e2aeae3068828679eabb36
Mukhtar Sibai <mukhtar@Mukhtars-MBP.local>
2023-11-18 16:05:24 +0100

    Create SignInDto class

---
+++ src/main/java/com/msibai/cloud/dtos/SignInDto.java
@@ -0,0 +1,15 @@
+package com.msibai.cloud.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignInDto {
+  private String email;
+  private String password;
+}
