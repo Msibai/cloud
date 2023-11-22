a5f193ff9bfb5abcf219ce6a185f0a97e80762ba
Mukhtar Sibai <mukhtar@Mukhtars-MBP.local>
2023-11-22 22:58:59 +0100

    refactor: Enhance SignUpDto validation and annotations

--- src/main/java/com/msibai/cloud/dtos/SignUpDto.java
+++ src/main/java/com/msibai/cloud/dtos/SignUpDto.java
@@ -2,7 +2,10 @@ package com.msibai.cloud.dtos;
 
 import com.msibai.cloud.annotations.PasswordMatches;
 import com.msibai.cloud.annotations.ValidEmail;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -11,20 +14,23 @@ import lombok.*;
 @AllArgsConstructor
 @PasswordMatches
 public class SignUpDto {
-  @NonNull
-  @NotEmpty
+  @NotBlank(message = "Name cannot be blank or contain only spaces")
+  @NotNull(message = "Name is required, please provide a valid name")
+  @Size(min = 3, max = 30, message = "Name length must be between 3 and 30 characters")
   private String firstName;
-  @NonNull
-  @NotEmpty
+  @NotBlank(message = "Last Name cannot be blank or contain only spaces")
+  @NotNull(message = "Last Name is required, please provide a valid name")
+  @Size(min = 3, max = 30, message = "Last length must be between 3 and 30 characters")
   private String lastName;
   @NonNull
   @NotEmpty
   @ValidEmail
   private String email;
-  @NonNull
-  @NotEmpty
+  @NotBlank(message = "Last Name cannot be blank or contain only spaces")
+  @NotNull(message = "Last Name is required, please provide a valid name")
+  @Size(min = 8, max = 30, message = "Invalid Password: Must be of 8 - 30 characters")
   private String password;
-  @NonNull
-  @NotEmpty
+  @NotNull(message = "Confirmation password cannot be null")
+  @NotEmpty(message = "Confirmation password cannot be empty")
   private String confirmPassword;
 }
