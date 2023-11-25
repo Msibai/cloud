3d1ee99f66e4e4d011b1032d4a2fd72897f81491
mukhtar <mukhtar.sibai@gmail.com>
2023-11-26 00:56:40 +0100

    Create FolderService interface for managing user-related folder operations

--- src/main/java/com/msibai/cloud/Services/FolderService.java
+++ src/main/java/com/msibai/cloud/Services/FolderService.java
@@ -1,2 +1,19 @@
-package com.msibai.cloud.Services;public class FolderService {
+package com.msibai.cloud.Services;
+
+import com.msibai.cloud.entities.Folder;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface FolderService {
+  void createFolderForUser(String folderName, String token);
+
+  Folder findFolderByIdAndUserId(UUID folderId, String token);
+
+  List<Folder> findAllFoldersByUserId(String token);
+
+  void updateFolderByIdAndUserId(UUID folderId, String token);
+
+  void deleteFolderByIdAndUserId(UUID folderId, String token);
 }
