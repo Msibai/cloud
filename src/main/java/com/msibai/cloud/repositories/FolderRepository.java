45af2d82d99fbabbb6bf3e37bbc7cf2c5009b2d4
mukhtar <mukhtar.sibai@gmail.com>
2023-11-26 00:33:45 +0100

    Add FolderRepository for CRUD operations on Folder entities

--- src/main/java/com/msibai/cloud/repositories/FolderRepository.java
+++ src/main/java/com/msibai/cloud/repositories/FolderRepository.java
@@ -1,2 +1,8 @@
-package com.msibai.cloud.repositories;public class FolderRepository {
-}
+package com.msibai.cloud.repositories;
+
+import com.msibai.cloud.entities.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface FolderRepository extends JpaRepository<Folder, UUID> {}
