609eb9deae98d7f3e0113c97cad505615343764a
mukhtar <mukhtar.sibai@gmail.com>
2023-11-28 19:26:28 +0100

    Define FileService interface for managing file operations

---
+++ src/main/java/com/msibai/cloud/Services/FileService.java
@@ -0,0 +1,15 @@
+package com.msibai.cloud.Services;
+
+import com.msibai.cloud.dtos.FileDto;
+import com.msibai.cloud.entities.File;
+import java.util.UUID;
+
+public interface FileService {
+  void uploadFileToFolder(String token, UUID folderId, FileDto file);
+
+  File downloadFileFromFolder(String token, UUID folderId, UUID fileId);
+
+  void deleteFileFromFolder(String token, UUID folderId, UUID fileId);
+
+  void moveFileToAnotherFolder(String token, UUID folderId, UUID fileId);
+}
