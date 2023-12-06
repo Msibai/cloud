08a02a022e27da67528ce324cd0641bcab68d352
mukhtar <mukhtar.sibai@gmail.com>
2023-12-06 21:52:38 +0100

    refactor: Add JavaDoc comments and inline comments in FolderController.

--- src/main/java/com/msibai/cloud/controllers/FolderController.java
+++ src/main/java/com/msibai/cloud/controllers/FolderController.java
@@ -18,42 +18,73 @@ public class FolderController {
 
   private final FolderServiceImpl folderServiceImpl;
 
+  /**
+   * Endpoint to create a new folder within a specified parent folder.
+   *
+   * @param user The authenticated user creating the folder.
+   * @param parentFolderId The ID of the parent folder.
+   * @param folderName The name of the new folder.
+   * @return ResponseEntity containing the status and message.
+   */
   @PostMapping("/{folder-id}/create")
   public ResponseEntity<String> createFolder(
       @AuthenticationPrincipal User user,
       @PathVariable("folder-id") UUID parentFolderId,
       String folderName) {
 
+    // Check if folderName is empty and return a bad request response
     if (folderName == null || folderName.isEmpty()) {
       return ResponseEntity.badRequest().body("Folder name cannot be empty");
     }
 
+    // Call service method to create a folder
     folderServiceImpl.createFolderForUser(user, parentFolderId, folderName);
 
+    // Return a success response with the folder creation message
     return ResponseEntity.status(HttpStatus.CREATED).body(folderName + " created successfully");
   }
 
+  /**
+   * Endpoint to retrieve sub folders of a specified folder.
+   *
+   * @param user The authenticated user requesting sub folders.
+   * @param folderId The ID of the folder to retrieve sub folders.
+   * @return ResponseEntity containing the list of FolderDto objects.
+   */
   @GetMapping("/{folder-id}")
   public ResponseEntity<List<FolderDto>> findSubFolders(
       @AuthenticationPrincipal User user, @PathVariable("folder-id") UUID folderId) {
 
+    // Call service method to find sub folders for the given folderId
     List<FolderDto> subFolders = folderServiceImpl.findSubFolders(user, folderId);
 
+    // Return the list of sub folders in a success response
     return ResponseEntity.ok(subFolders);
   }
 
+  /**
+   * Endpoint to update the name of a folder.
+   *
+   * @param user The authenticated user updating the folder name.
+   * @param folderId The ID of the folder to be updated.
+   * @param newFolderName The new name for the folder.
+   * @return ResponseEntity containing the status and message.
+   */
   @PutMapping("/{folder-id}/update")
   public ResponseEntity<String> updateFolderName(
       @AuthenticationPrincipal User user,
       @PathVariable("folder-id") UUID folderId,
       @RequestParam("new-folder-name") String newFolderName) {
 
+    // Check if newFolderName is empty and return a bad request response
     if (newFolderName == null || newFolderName.isEmpty()) {
       return ResponseEntity.badRequest().body("Invalid new folder name.");
     }
 
+    // Call service method to update the folder name
     folderServiceImpl.updateFolderName(user, folderId, newFolderName);
 
+    // Return a success response with the update confirmation message
     return ResponseEntity.ok("Folder name updated successfully");
   }
 }
