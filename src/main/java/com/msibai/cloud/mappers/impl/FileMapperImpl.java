2f8eb8c3658ed7cf9c8804a5d80df395c117c20d
mukhtar <mukhtar.sibai@gmail.com>
2023-12-07 20:03:46 +0100

    Implement FileMapper for mapping between File and FileDto

---
+++ src/main/java/com/msibai/cloud/mappers/impl/FileMapperImpl.java
@@ -0,0 +1,25 @@
+package com.msibai.cloud.mappers.impl;
+
+import com.msibai.cloud.dtos.FileDto;
+import com.msibai.cloud.entities.File;
+import com.msibai.cloud.mappers.Mapper;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class FileMapperImpl implements Mapper<File, FileDto> {
+
+  private final ModelMapper modelMapper;
+
+  @Override
+  public FileDto mapTo(File file) {
+    return modelMapper.map(file, FileDto.class);
+  }
+
+  @Override
+  public File mapFrom(FileDto fileDto) {
+    return modelMapper.map(fileDto, File.class);
+  }
+}
