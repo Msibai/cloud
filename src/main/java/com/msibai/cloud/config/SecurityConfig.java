b52c5f90b30f084caad7d16535539f251ea6d295
mukhtar <mukhtar.sibai@gmail.com>
2023-12-09 20:08:38 +0100

    docs: Add JavaDoc and inline comments to SecurityConfig.

--- src/main/java/com/msibai/cloud/config/SecurityConfig.java
+++ src/main/java/com/msibai/cloud/config/SecurityConfig.java
@@ -21,25 +21,37 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
+/** Configuration class for setting up security configurations. */
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
   private final UserService userService;
-
   private final HandlerExceptionResolver exceptionResolver;
 
+  /**
+   * Constructor to initialize UserService and HandlerExceptionResolver.
+   *
+   * @param userService UserService instance.
+   * @param exceptionResolver HandlerExceptionResolver instance.
+   */
   public SecurityConfig(
       UserService userService,
       @Qualifier("handlerExceptionResolver") HandlerExceptionResolver exceptionResolver) {
-
     this.userService = userService;
     this.exceptionResolver = exceptionResolver;
   }
 
+  /**
+   * Configures the security filter chain.
+   *
+   * @param http HttpSecurity object for configuring security.
+   * @return SecurityFilterChain instance.
+   * @throws Exception If an error occurs during configuration.
+   */
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
+    return http.csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
@@ -50,13 +62,17 @@ public class SecurityConfig {
                     .anyRequest()
                     .authenticated())
         .authenticationProvider(authenticationProvider())
-        .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    return http.build();
+        .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+        .build();
   }
 
+  /**
+   * Creates an AuthenticationProvider bean for authentication.
+   *
+   * @return AuthenticationProvider instance.
+   */
   @Bean
   public AuthenticationProvider authenticationProvider() {
-
     DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
     authProvider.setUserDetailsService(userService);
     authProvider.setPasswordEncoder(passwordEncoder());
@@ -64,16 +80,33 @@ public class SecurityConfig {
     return authProvider;
   }
 
+  /**
+   * Creates a JwtAuthenticationFilter bean for processing JWT authentication.
+   *
+   * @return JwtAuthenticationFilter instance.
+   */
   @Bean
   public JwtAuthenticationFilter jwtAuthenticationFilter() {
     return new JwtAuthenticationFilter(exceptionResolver);
   }
 
+  /**
+   * Creates a PasswordEncoder bean for encoding passwords.
+   *
+   * @return PasswordEncoder instance.
+   */
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 
+  /**
+   * Creates an AuthenticationManager bean for authentication purposes.
+   *
+   * @param config AuthenticationConfiguration for obtaining the AuthenticationManager.
+   * @return AuthenticationManager instance.
+   * @throws Exception If an error occurs while obtaining the AuthenticationManager.
+   */
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
       throws Exception {
