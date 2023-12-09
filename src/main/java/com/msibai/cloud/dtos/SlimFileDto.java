ce060b11e1f81ddd9fbb98d1cf269510ed05d100
mukhtar <mukhtar.sibai@gmail.com>
2023-12-09 19:40:08 +0100

    docs: Add JavaDoc and inline comments to SlimFileDto.

--- src/main/java/com/msibai/cloud/dtos/SlimFileDto.java
+++ src/main/java/com/msibai/cloud/dtos/SlimFileDto.java
@@ -6,13 +6,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/** Data Transfer Object (DTO) representing essential information about a file. */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class SlimFileDto {
-  private UUID id;
-  private String name;
-  private String contentType;
-  private Long size;
+
+  private UUID id; // Unique identifier for the file
+
+  private String name; // Name of the file
+
+  private String contentType; // MIME type of the file content
+
+  private Long size; // Size of the file in bytes
 }
