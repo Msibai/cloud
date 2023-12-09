96544760cc5b5580eb9b0dc7faaa4a913a0d5cbe
mukhtar <mukhtar.sibai@gmail.com>
2023-12-09 21:34:47 +0100

    docs: Add JavaDoc and inline comments to FolderMapperImpl.

--- src/main/java/com/msibai/cloud/mappers/impl/FolderMapperImpl.java
+++ src/main/java/com/msibai/cloud/mappers/impl/FolderMapperImpl.java
@@ -7,17 +7,30 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+/** Implementation of Mapper interface for mapping between Folder entity and FolderDto DTO. */
 @AllArgsConstructor
 @Component
 public class FolderMapperImpl implements Mapper<Folder, FolderDto> {
 
   private final ModelMapper modelMapper;
 
+  /**
+   * Maps a Folder entity to a FolderDto.
+   *
+   * @param folder The Folder entity to be mapped.
+   * @return The corresponding FolderDto.
+   */
   @Override
   public FolderDto mapTo(Folder folder) {
     return modelMapper.map(folder, FolderDto.class);
   }
 
+  /**
+   * Maps a FolderDto to a Folder entity.
+   *
+   * @param folderDto The FolderDto to be mapped.
+   * @return The corresponding Folder entity.
+   */
   @Override
   public Folder mapFrom(FolderDto folderDto) {
     return modelMapper.map(folderDto, Folder.class);
