efec07bc11db530ff616850dd8ff4ce518208eae
mukhtar <mukhtar.sibai@gmail.com>
2023-12-09 20:31:18 +0100

    docs: Add JavaDoc and inline comments to Folder entity.

--- src/main/java/com/msibai/cloud/entities/Folder.java
+++ src/main/java/com/msibai/cloud/entities/Folder.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 import lombok.*;
 
+/** Entity class representing a folder stored in the system. */
 @Data
 @Builder
 @Entity
@@ -14,24 +15,26 @@ import lombok.*;
 @AllArgsConstructor
 @Table(name = "folders")
 public class Folder {
+
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID id;
+  private UUID id; // Unique identifier for the folder
 
   @NonNull
-  @NotEmpty(message = "Folder must have name")
+  @NotEmpty(message = "Folder must have a name")
   @Pattern(
       regexp = "^([a-zA-Z0-9][^*/><?\\\\|:]*)$",
       message =
           "Folder name is not valid. It should start with letters or numbers "
               + "and can contain any characters except '*', '/', '>', '<', '?', '|', or ':'.")
-  private String folderName;
+  private String folderName; // Name of the folder
 
-  private UUID parentFolderId;
-  @NonNull private UUID userId;
+  private UUID parentFolderId; // ID of the parent folder
+
+  @NonNull private UUID userId; // ID of the user associated with the folder
 
   @Column(nullable = false)
-  private boolean isRootFolder;
+  private boolean isRootFolder; // Flag indicating if this folder is the root folder
 
-  @NonNull private LocalDate creationDate;
+  @NonNull private LocalDate creationDate; // Date when the folder was created
 }
