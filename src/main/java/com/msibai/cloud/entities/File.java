785fa1730e155803decd7c5e04530821d56bdc98
mukhtar <mukhtar.sibai@gmail.com>
2023-12-09 19:40:53 +0100

    docs: Add JavaDoc and inline comments to File entity.

--- src/main/java/com/msibai/cloud/entities/File.java
+++ src/main/java/com/msibai/cloud/entities/File.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import java.util.UUID;
 import lombok.*;
 
+/** Entity class representing a file stored in the system. */
 @Data
 @Builder
 @Entity
@@ -11,25 +12,26 @@ import lombok.*;
 @AllArgsConstructor
 @Table(name = "files")
 public class File {
+
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID id;
+  private UUID id; // Unique identifier for the file
 
   @NonNull
   @Column(unique = true)
-  private String name;
+  private String name; // Name of the file
 
-  @NonNull private String contentType;
+  @NonNull private String contentType; // MIME type of the file content
 
-  private byte @NonNull [] content;
+  private byte @NonNull [] content; // Actual content of the file
 
-  private long size;
+  private long size; // Size of the file in bytes
 
-  @NonNull private UUID userId;
+  @NonNull private UUID userId; // ID of the user associated with the file
 
-  @NonNull private UUID folderId;
+  @NonNull private UUID folderId; // ID of the folder where the file is stored
 
-  @NonNull private String encryptionKey;
+  @NonNull private String encryptionKey; // Encryption key used for the file
 
-  @NonNull private String iv;
+  @NonNull private String iv; // Initialization Vector (IV) for encryption
 }
