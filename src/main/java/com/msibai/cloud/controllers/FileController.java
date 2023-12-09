3ae353a963c3b06dc091d2549a27b104e210a5d9
mukhtar <mukhtar.sibai@gmail.com>
2023-12-09 15:27:46 +0100

    docs: Add JavaDoc and inline comments to FileController

--- src/main/java/com/msibai/cloud/controllers/FileController.java
+++ src/main/java/com/msibai/cloud/controllers/FileController.java
@@ -25,6 +25,16 @@ public class FileController {
 
   private final FileServiceImpl fileServiceImpl;
 
+  /**
+   * Endpoint to upload a file to a specific folder.
+   *
+   * @param user The authenticated user.
+   * @param folderId The ID of the folder where the file will be uploaded.
+   * @param file The file to be uploaded.
+   * @return ResponseEntity indicating the status of the file upload.
+   * @throws IOException If an I/O error occurs.
+   * @throws NoSuchAlgorithmException If a requested cryptographic algorithm is not available.
+   */
   @PostMapping("/{folder-id}/upload")
   public ResponseEntity<String> uploadFile(
       @AuthenticationPrincipal User user,
@@ -32,14 +42,17 @@ public class FileController {
       @RequestParam("file") MultipartFile file)
       throws IOException, NoSuchAlgorithmException {
 
+    // Check if the file is missing
     if (file == null) {
       return ResponseEntity.badRequest().body("File is missing");
     }
 
-    if (file.getSize() > 2097152000) {
+    // Check if the file exceeds the size limit
+    if (file.getSize() > 209715200) {
       return ResponseEntity.badRequest().body("File size exceeds the limit");
     }
 
+    // Construct FileDto object with file details
     FileDto uploadedFile =
         FileDto.builder()
             .name(file.getOriginalFilename())
@@ -48,11 +61,22 @@ public class FileController {
             .size(file.getSize())
             .build();
 
+    // Call service method to upload the file
     fileServiceImpl.uploadFileToFolder(user, folderId, uploadedFile);
 
     return ResponseEntity.ok(file.getOriginalFilename() + " uploaded successfully!");
   }
 
+  /**
+   * Endpoint to retrieve files in a specific folder with pagination.
+   *
+   * @param user The authenticated user.
+   * @param folderId The ID of the folder to retrieve files from.
+   * @param page The page number for pagination (default: 0).
+   * @param size The page size for pagination (default: 10).
+   * @return ResponseEntity containing PagedResponse with files in the folder.
+   * @throws InvalidPaginationParameterException If pagination parameters are invalid.
+   */
   @GetMapping("{folder-id}/files")
   public ResponseEntity<PagedResponse<SlimFileDto>> findFilesInFolder(
       @AuthenticationPrincipal User user,
@@ -60,13 +84,17 @@ public class FileController {
       @RequestParam(defaultValue = "0") int page,
       @RequestParam(defaultValue = "10") int size) {
 
+    // Validate pagination parameters
     if (page < 0 || size <= 0) {
       throw new InvalidPaginationParameterException("Invalid pagination parameters.");
     }
 
     Pageable pageable = PageRequest.of(page, size);
+
+    // Retrieve files from the folder using the service method
     Page<SlimFileDto> filesPage = fileServiceImpl.findByFolderId(user, folderId, pageable);
 
+    // Create a PagedResponse and set headers for pagination details
     PagedResponse<SlimFileDto> response =
         new PagedResponse<>(
             filesPage.getContent(),
@@ -81,10 +109,18 @@ public class FileController {
     return ResponseEntity.ok().headers(headers).body(response);
   }
 
+  /**
+   * Endpoint to download a specific file from a folder.
+   *
+   * @param user The authenticated user.
+   * @param fileId The ID of the file to be downloaded.
+   * @return ResponseEntity with the file content for download.
+   */
   @GetMapping("/{folder-id}/{file-id}/download")
   public ResponseEntity<byte[]> downloadFile(
       @AuthenticationPrincipal User user, @PathVariable("file-id") UUID fileId) {
 
+    // Retrieve file details using the service method
     FileDto file = fileServiceImpl.downloadFile(user, fileId);
     if (file == null) {
       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -94,6 +130,7 @@ public class FileController {
     String fileName = file.getName();
     String contentType = file.getContentType();
 
+    // Set headers for file download
     HttpHeaders headers = new HttpHeaders();
     headers.setContentDisposition(ContentDisposition.attachment().filename(fileName).build());
     headers.setContentType(MediaType.parseMediaType(contentType));
@@ -101,23 +138,41 @@ public class FileController {
     return new ResponseEntity<>(fileData, headers, HttpStatus.OK);
   }
 
+  /**
+   * Endpoint to delete a specific file from a folder.
+   *
+   * @param user The authenticated user.
+   * @param fileId The ID of the file to be deleted.
+   * @return ResponseEntity indicating the status of the file deletion.
+   */
   @DeleteMapping("/{folder-id}/{file-id}/delete")
   public ResponseEntity<String> deleteFile(
       @AuthenticationPrincipal User user, @PathVariable("file-id") UUID fileId) {
 
+    // Call service method to delete the file
     fileServiceImpl.deleteFile(user, fileId);
 
     return ResponseEntity.ok("File deleted successfully");
   }
 
+  /**
+   * Endpoint to move a file to another folder.
+   *
+   * @param user The authenticated user.
+   * @param currentFolderId The ID of the current folder containing the file.
+   * @param fileId The ID of the file to be moved.
+   * @param targetFolderId The ID of the target folder for moving the file.
+   * @return ResponseEntity indicating the status of the file move operation.
+   */
   @PutMapping("/{current-folder-id}/{file-id}/move")
   public ResponseEntity<String> moveFileToAnotherFolder(
       @AuthenticationPrincipal User user,
       @PathVariable("current-folder-id") UUID currentFolderId,
       @PathVariable("file-id") UUID fileId,
       @RequestParam UUID targetFolderId) {
-
+    // Call service method to move the file to the target folder
     fileServiceImpl.moveFileToAnotherFolder(user, currentFolderId, fileId, targetFolderId);
+
     return ResponseEntity.ok("File moved successfully to the target folder.");
   }
 }
