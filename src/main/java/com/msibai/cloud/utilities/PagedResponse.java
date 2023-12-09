fe6be4183c0ed5c340e64e859490cd79198f5d31
mukhtar <mukhtar.sibai@gmail.com>
2023-12-09 15:57:16 +0100

    docs: Add JavaDoc and inline comments to PagedResponse.

--- src/main/java/com/msibai/cloud/utilities/PagedResponse.java
+++ src/main/java/com/msibai/cloud/utilities/PagedResponse.java
@@ -4,12 +4,17 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Represents a paginated response containing a list of elements along with pagination metadata.
+ *
+ * @param <T> The type of elements in the response.
+ */
 @Data
 @AllArgsConstructor
 public class PagedResponse<T> {
-  private List<T> content;
-  private int page;
-  private int size;
-  private long totalElements;
-  private int totalPages;
+  private List<T> content; // List of elements in the current page
+  private int page; // Current page number
+  private int size; // Number of elements in the page
+  private long totalElements; // Total number of elements across all pages
+  private int totalPages; // Total number of pages
 }
