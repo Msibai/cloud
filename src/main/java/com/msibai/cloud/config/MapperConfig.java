c039f874b58e68e99fc515589e6eeb3918a1c30b
mukhtar <mukhtar.sibai@gmail.com>
2023-12-09 20:04:08 +0100

    docs: Add JavaDoc and inline comments to MapperConfig.

--- src/main/java/com/msibai/cloud/config/MapperConfig.java
+++ src/main/java/com/msibai/cloud/config/MapperConfig.java
@@ -4,9 +4,15 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/** Configuration class for creating and configuring the ModelMapper bean. */
 @Configuration
 public class MapperConfig {
 
+  /**
+   * Creates a bean for ModelMapper.
+   *
+   * @return The configured ModelMapper bean.
+   */
   @Bean
   public ModelMapper modelMapper() {
     return new ModelMapper();
